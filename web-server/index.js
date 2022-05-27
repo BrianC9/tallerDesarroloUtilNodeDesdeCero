60a3b7b6372665279572f271a20007609f6cd085
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-05-27 17:54:27 +0200

    filtrando requests HTTP

--- web-server/index.js
+++ web-server/index.js
@@ -2,10 +2,22 @@ console.clear()
 import { createServer } from 'http'
 
 const server = createServer((req, res) => {
+    let data = ''
+    let chunkIndex = 0
+    req.setEncoding('utf-8')
     if (req.method === 'POST') {
-        res.end("Recibido POST method")
+
+        req.on('data', (chunk) => {
+            data += chunk;
+            chunkIndex++
+            console.log(chunkIndex, chunk)
+        })
+        req.on('end', () => {
+            console.log("Recibido POST method", data.toString())
+        })
+
     } else if (req.method !== 'POST') {
-        res.end("Recibido otro method")
+        res.end(req.method)
     }
     console.log('PETICION RECIBIDA')
 
