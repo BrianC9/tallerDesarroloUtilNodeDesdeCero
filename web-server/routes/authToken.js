3de536cc3b781160a371d244f1750aa0ed14ae7f
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-06-14 20:10:08 +0200

    Autorización y autenticación con JWT

--- web-server/routes/authToken.js
+++ web-server/routes/authToken.js
@@ -1,17 +1,44 @@
 import { Router } from "express";
-import checkEmailPassword from "../helpers/checkEmailPasswd.js";
+import checkEmailPasswd from "../helpers/checkEmailPasswd.js";
+import { SignJWT, jwtVerify } from 'jose'
+import { USERS_BBDD } from "../bbdd.js";
 const authTokenRouter = Router();
-
+const encoder = new TextEncoder()
 // Login con email y password
-authTokenRouter.post('/login', (req, res) => {
+authTokenRouter.post('/login', async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).end("Tienes que introducir email y password")
     try {
-        const user = checkEmailPasswd(email, password)
+        const { guid } = checkEmailPasswd(email, password)
 
-        return res.end("Autenticacion correcta de " + user.name)
+        const jwtConstructor = new SignJWT({ guid });
+
+
+        const jwt = await jwtConstructor
+            .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+            .setIssuedAt()
+            .setExpirationTime('1h').sign(encoder.encode(process.env.JWT_PRIVATE_KEY))
+
+        return res.send({ jwt })
     } catch (error) {
         return res.status(401).end(error.message)
     }
+})
+authTokenRouter.get("/mi-perfil", async (req, res) => {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(401).end("No se ha especificado la cabecera authorization")
+    try {
+
+        const { payload } = await jwtVerify(authorization, encoder.encode(process.env.JWT_PRIVATE_KEY))
+        console.log(payload)
+        const user = USERS_BBDD.find(user => user.guid === payload.guid)
+        if (!user) return res.status(401).send("No se ha encontrado al usuario")
+        delete user.password
+        return res.send(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(401).end("Error al verificar el jwt\n" + error.message)
+    }
+
 })
 export default authTokenRouter;
